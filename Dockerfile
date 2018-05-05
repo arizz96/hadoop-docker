c02e7bc0650f54fadb05bc6e65bda818223048ec
arizz96 <arizz96@gmail.com>
2018-05-05 15:01:35 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM java:openjdk-8u111-jdk
+RUN apt-get update -qq && apt-get install -y build-essential software-properties-common build-essential pkg-config xmlstarlet
+
+# Hadoop installation
+RUN curl -fSL "http://www-eu.apache.org/dist/hadoop/common/KEYS" -o /tmp/KEYS \
+  && gpg --import /tmp/KEYS
+ENV HADOOP_VERSION 3.1.0
+RUN curl -fSL "http://it.apache.contactlab.it/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz" -o /tmp/hadoop.tar.gz \
+    && curl -fSL "http://www-eu.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz.asc" -o /tmp/hadoop.tar.gz.asc \
+    && gpg --verify /tmp/hadoop.tar.gz.asc \
+    && tar -xvf /tmp/hadoop.tar.gz -C /opt/ \
+    && rm /tmp/hadoop.tar.gz* \
+    && ln -s /opt/hadoop-$HADOOP_VERSION/etc/hadoop /etc/hadoop \
+    && mkdir /opt/hadoop-$HADOOP_VERSION/logs \
+    && mkdir /hadoop-data \
+    && rm -Rf /opt/hadoop-$HADOOP_VERSION/share/doc/hadoop
+
+ENV HADOOP_HOME=/opt/hadoop-$HADOOP_VERSION
+ENV HADOOP_CONF_DIR=/etc/hadoop
+
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod a+x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+: ${HADOOP_ROLE:?"HADOOP_ROLE is required and should be namenode, datanode or journal."}
+
+# Set some sensible defaults
+export CORE_CONF_fs_defaultFS=${CORE_CONF_fs_defaultFS:-hdfs://`hostname -f`:8020}
+
+function addProperty() {
+  local path=$1
+  local name=$2
+  local value=$3
+
+  if [ $# -ne 3 ]; then
+      echo "There should be 3 arguments to addConfig: <file-to-modify.xml>, <property>, <value>"
+      echo "Given: $@"
+      exit 1
+  fi
+
+  xmlstarlet ed -L -s "/configuration" -t elem -n propertyTMP -v "" \
+   -s "/configuration/propertyTMP" -t elem -n name -v $name \
+   -s "/configuration/propertyTMP" -t elem -n value -v $value \
+   -r "/configuration/propertyTMP" -v "property" \
+   $path
+}
+
+function configure() {
+    local path=$1
+    local module=$2
+    local envPrefix=$3
+
+    local var
+    local value
+
+    echo "Configuring $module"
+    for c in `printenv | perl -sne 'print "$1 " if m/^${envPrefix}_(.+?)=.*/' -- -envPrefix=$envPrefix`; do
+        name=`echo ${c} | perl -pe 's/___/-/g; s/__/_/g; s/_/./g'`
+        var="${envPrefix}_${c}"
+        value=${!var}
+        echo " - Setting $name=$value"
+        addProperty /etc/hadoop/$module-site.xml $name "$value"
+    done
+}
+
+function configureHostResolver() {
+    sed -i "/hosts:/ s/.*/hosts: $*/" /etc/nsswitch.conf
+}
+
+configure $HADOOP_CONF_DIR/core-site.xml core CORE_CONF
+configure $HADOOP_CONF_DIR/hdfs-site.xml hdfs HDFS_CONF
+configure $HADOOP_CONF_DIR/yarn-site.xml yarn YARN_CONF
+configure $HADOOP_CONF_DIR/httpfs-site.xml httpfs HTTPFS_CONF
+configure $HADOOP_CONF_DIR/kms-site.xml kms KMS_CONF
+
+# start node
+if [[ ${HADOOP_ROLE,,} = namenode ]]; then
+    source roles/namenode.sh
+elif [[ ${HADOOP_ROLE,,} = datanode ]]; then
+    source roles/datanode.sh
+elif [[ ${HADOOP_ROLE,,} = journalnode ]]; then
+    source roles/journalnode.sh
+else
+    echo "HADOOP_ROLE's value must be one of: namenode, datanode or journalnode"
+    exit 1
+fi
